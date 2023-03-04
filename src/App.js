b7d92f06130e4de84209646afd2fc7f630581e72
Tedy Khoirul Abidin <tedikhoirulabidin@gmail.com>
2023-03-04 21:09:40 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LandingPage from "./pages/LandingPage";
 import './assets/scss/style.scss';
 
 function App() {
   return (
     <div className="App">
-      Hai dunia!
+        <Router>
+            <Route path="/" component={LandingPage} />
+        </Router>
     </div>
   );
 }
