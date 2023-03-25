374bea58dde5b2eb3cdb539e3ba1d227b07280bc
Tedy Khoirul Abidin <tedikhoirulabidin@gmail.com>
2023-03-25 11:23:08 +0700

    create pageDetailTitle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import LandingPage from "./pages/LandingPage";
+import LandingPage from "pages/LandingPage";
+import DetailsPage from "pages/DetailsPage";
 import './assets/scss/style.scss';
 
 function App() {
   return <div className="App">
     <Router>
-        <Route path="/" component={LandingPage}></Route>
+        <Route exact path="/" component={LandingPage}></Route>
+        <Route exact path="/properties/:id" component={DetailsPage}></Route>
     </Router>
   </div>
 };
