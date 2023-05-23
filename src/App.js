c6889c280076c3a1b4d144250f7934616cde532d
Tedy Khoirul Abidin <tedikhoirulabidin@gmail.com>
2023-05-23 10:36:04 +0700

    create stepper component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import LandingPage from "pages/LandingPage";
 import DetailsPage from "pages/DetailsPage";
+import CheckoutPage from "pages/Checkout"
+
 import './assets/scss/style.scss';
 
 function App() {
   return <div className="App">
     <Router>
-        <Route exact path="/" component={LandingPage}></Route>
-        <Route exact path="/properties/:id" component={DetailsPage}></Route>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/properties/:id" component={DetailsPage} />
+        <Route path="/checkout" component={CheckoutPage} />
     </Router>
   </div>
 };
